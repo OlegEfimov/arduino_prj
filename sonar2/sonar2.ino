47a1b44732b49e022b29a0824565acb07251e7eb
Efimov Oleg <efimoff.oleg@gmail.com>
2018-07-10 01:01:20 +0300

    try engine start

--- sonar2/sonar2.ino
+++ sonar2/sonar2.ino
@@ -11,19 +11,40 @@
  */
 #include <stdio.h>
 #include "pins_arduino.h"
- 
+//////////////////////
+// motor setup
+// http://zelectro.cc/Motor_shield_L298N_Arduino
+////////////////////// 
+int ENA = 9;
+int IN1 = 7;
+int IN2 = 6;
+int IN3 = 5;
+int IN4 = 4;
+int ENB = 3;
+int i;
+///////////////////////
+
 int trigPin0 = 22;    // Trig – зеленый провод-перемычка
 int echoPin0 = 23;    // Echo – желтый провод-перемычка
 int trigPin1 = 24;    // Trig – зеленый провод-перемычка
 int echoPin1 = 25;    // Echo – желтый провод-перемычка
 int trigPin2 = 26;    // Trig – зеленый провод-перемычка
 int echoPin2 = 27;    // Echo – желтый провод-перемычка
-int trigPin3 = 6;
-int echoPin3 = 7;
-int trigPin4 = 8;
-int echoPin4 = 9;
-int trigPin5 = 10;
-int echoPin5 = 11;
+
+int trigPin3 = 28;
+int echoPin3 = 29;
+int trigPin4 = 30;
+int echoPin4 = 31;
+int trigPin5 = 32;
+int echoPin5 = 33;
+
+//int trigPin3 = 6;
+//int echoPin3 = 7;
+//int trigPin4 = 8;
+//int echoPin4 = 9;
+//int trigPin5 = 10;
+//int echoPin5 = 11;
+
 //int trigPin6 = 12;
 //int echoPin6 = 13;
 //unsigned long cm;
@@ -55,6 +76,15 @@ int lenght_out;
 //int dbgvalue5;
 
 void setup() {
+// motor
+ pinMode (ENA, OUTPUT); 
+ pinMode (IN1, OUTPUT);
+ pinMode (IN2, OUTPUT);
+ pinMode (IN3, OUTPUT);
+ pinMode (IN4, OUTPUT);
+ pinMode (ENB, OUTPUT); 
+
+
 //  dbgvalue1 = 0;
 //  dbgvalue2 = 0;
 //  dbgvalue3 = 0;
@@ -247,4 +277,50 @@ void loop()
   delay(200);
 //    Serial3.write(0);
 //  delay(250);
+
+/////////////////////////////
+//  // На пару выводов "IN" поданы разноименные сигналы, мотор готов к вращаению
+//  digitalWrite (IN3, HIGH);
+//  digitalWrite (IN4, LOW);
+//  // подаем на вывод ENB управляющий ШИМ сигнал 
+//  analogWrite(ENB,155);
+//  delay(2000);
+//  analogWrite(ENB,205);
+//  delay(2000);
+//  analogWrite(ENB,255);
+//  delay(2000);
+//  // Останавливаем мотор повад на вывод ENB сигнал низкого уровеня. 
+//  // Состояние выводов "IN" роли не играет.
+//  analogWrite(ENB,0);
+//  delay(5000);
+//
+///////////////////////
+  digitalWrite (IN2, HIGH);
+  digitalWrite (IN1, LOW); 
+  digitalWrite (IN4, HIGH);
+  digitalWrite (IN3, LOW); 
+  for (i = 50; i <= 180; ++i)
+  {
+      analogWrite(ENA, i);
+      analogWrite(ENB, i);
+      delay(30);
+  }
+  analogWrite (ENA, 0);
+  analogWrite (ENB, 0);
+  delay(500);
+  digitalWrite (IN1, HIGH);
+  digitalWrite (IN2, LOW); 
+  digitalWrite (IN3, HIGH);
+  digitalWrite (IN4, LOW);
+  for (i = 50; i <= 180; ++i)
+  {
+      analogWrite(ENA, i);
+      analogWrite(ENB, i);
+      delay(30);
+  }
+  analogWrite (ENA, 0);
+  analogWrite (ENB, 0);
+  delay(8000);
+
+///////////////////////
 }
