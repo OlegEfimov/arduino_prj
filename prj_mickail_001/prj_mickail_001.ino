61db5d11ca5731083456aafa40c878b71fffeb21
ruoefimov <oleg.efimov@harman.com>
2020-02-03 21:27:08 +0300

    update + docs

--- prj_mickail_001/prj_mickail_001.ino
+++ prj_mickail_001/prj_mickail_001.ino
@@ -1,21 +1,24 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x3f,16,2); // Указываем I2C адрес (наиболее распространенное значение), а также параметры экрана (в случае LCD 1602 - 2 строки по 16 символов в каждой
+LiquidCrystal_I2C lcd(0x3f,16,2); // Указываем I2C адрес = 0x3f
+                                  // параметры экрана - 2 строки по 16 символов в каждой
 const int trigPin = 9;
 const int echoPin = 8;
+const int CYCLE_NUMBER = 10;
 long duration;
 long sumDuration;
-int distanceCm, distanceInch, counter;
+int counter;
 
 void setup() {
   counter = 0;
   sumDuration = 0;
+  
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0,0);
-  lcd.print("Hello");
+  lcd.print("---Instant value");
   lcd.setCursor(0,1);
-  lcd.print("ArduinoMaster");
+  lcd.print("---Average value");
   delay(3000);
   lcd.clear();
 
@@ -29,20 +32,18 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
-//  distanceCm= duration*0.0343/2;
-//  distanceInch = duration*0.0133/2;
 
   lcd.setCursor(0,0);
-  lcd.print("t: ");
+  lcd.print("inst: ");
   lcd.print(duration);
   lcd.print(" mks      ");
-  delay(10);
+
   counter += 1;
   sumDuration += duration;
-  if (counter >= 10) {
+  if (counter >= CYCLE_NUMBER) {
     lcd.setCursor(0,1);
-    lcd.print("t: ");
-    lcd.print(sumDuration / 10);
+    lcd.print("mean: ");
+    lcd.print(sumDuration / CYCLE_NUMBER);
     lcd.print(" mks        ");
     sumDuration = 0;
     counter = 0;
